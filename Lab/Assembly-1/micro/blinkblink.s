087df2acc5da9517e9b5d5d77df93c3f011a7280
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2021-12-30 18:25:37 +0330

    Completed Project

--- Lab/Assembly-1/micro/blinkblink.s
+++ Lab/Assembly-1/micro/blinkblink.s
@@ -2,6 +2,7 @@ PIO_PER EQU 0x400E0E00
 PIO_OER EQU 0x400E0E10
 PIO_SODR EQU 0x400E0E30
 PIO_CODR EQU 0x400E0E34
+
 PIO_ISR EQU  0x400E0E4C
 
 	AREA myCode, CODE, READONLY
@@ -12,9 +13,10 @@ __main
 	BL enable_pio1
 	BL enable_pio2
 	BL enable_pio3
+	LDR r6, =0x00A0000	; Delay
 
 loop
-	
+	; Pulling for Reset
 	LDR r1, =PIO_ISR
 	LDR r2, [r1]
 	CMP r2, #2_1000000
@@ -24,8 +26,9 @@ loop
 	BL led_on1		; On
 	BL delay		; Delay
 	BL led_off1		; Off
-	BL delay		; Delay	
+	BL delay		; Delay
 	
+	; Pulling for Reset
 	LDR r1, =PIO_ISR
 	LDR r2, [r1]
 	CMP r2, #2_1000000
@@ -37,6 +40,7 @@ loop
 	BL led_off2		; Off
 	BL delay		; Delay	
 	
+	; Pulling for Reset
 	LDR r1, =PIO_ISR
 	LDR r2, [r1]
 	CMP r2, #2_1000000
@@ -64,7 +68,7 @@ over
 	B hold
 
 enable_pio1
-	MOV R4, #2_1	
+	MOV R4, #2_1		; 36
 	LDR R5, =PIO_PER
 	STR R4, [R5]	
 	LDR R5, =PIO_OER
@@ -72,7 +76,7 @@ enable_pio1
 	BX LR
 
 enable_pio2
-	MOV R4, #2_10
+	MOV R4, #2_10		; 35
 	LDR R5, =PIO_PER
 	STR R4, [R5]	
 	LDR R5, =PIO_OER
@@ -80,7 +84,7 @@ enable_pio2
 	BX LR
 
 enable_pio3
-	MOV R4, #2_100
+	MOV R4, #2_100		; 32
 	LDR R5, =PIO_PER
 	STR R4, [R5]	
 	LDR R5, =PIO_OER
@@ -88,44 +92,46 @@ enable_pio3
 	BX LR
 
 led_on1
-	MOV R4, #2_1
+	MOV R4, #2_1		; 36
 	LDR R5, =PIO_SODR
 	STR R4, [R5]
 	BX LR
 
 led_on2
-	MOV R4, #2_10
+	MOV R4, #2_10		; 35
 	LDR R5, =PIO_SODR
 	STR R4, [R5]
 	BX LR
 
 led_on3
-	MOV R4, #2_100
+	MOV R4, #2_100		; 32
 	LDR R5, =PIO_SODR
 	STR R4, [R5]
 	BX LR
 
 led_off1
-	MOV R4, #2_1
+	MOV R4, #2_1		; 36
 	LDR R5, =PIO_CODR
 	STR R4, [R5]	
 	BX LR
 
 led_off2
-	MOV R4, #2_10
+	MOV R4, #2_10		; 35
 	LDR R5, =PIO_CODR
 	STR R4, [R5]	
 	BX LR
 
 led_off3
-	MOV R4, #2_100
+	MOV R4, #2_100		; 32
 	LDR R5, =PIO_CODR
 	STR R4, [R5]	
 	BX LR
 
 delay
 	MOV R4, #0
-	LDR R5, =0x00A0000
+	SUBS R6, R6, #0x000A000	; Decrement delay (10%)
+	MOVLS R6, #0x00A0000	; Reset delay if is less than 0
+	MOV R5, R6
 
 delay_loop
 	ADD R4, R4, #1
